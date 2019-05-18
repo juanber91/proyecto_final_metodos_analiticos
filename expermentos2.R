ee2836e00ff4d031272157814a7debf430789241
Juan Mpc <jbmpc@outlook.com>
2019-05-17 20:36:11 -0500

    mas experimentos

---
+++ expermentos2.R
@@ -0,0 +1,117 @@
+library('tidyverse')
+library('RgoogleMaps')
+library('readxl')
+library('rgdal')
+library('stplanr')
+library('rjson') 
+library('ggmap')
+
+# importamos los datos de las rutas
+datos <- read_excel("data/rutas-y-corredores-del-transporte-publico-concesionado.xlsx")
+
+# Lo mismo pero en shapefile, veremos con cuál de los dos conviene trabajar
+datos.sf <- 
+  readOGR("data/prueba/rutas-y-corredores-del-transporte-publico-concesionado.shp")
+
+rawplot <- plot(datos.sf)
+
+
+#### ---------------------------------------------------------------------
+#### La idea aquí es ir haciendo pruebas con subconjuntos de los datos,
+#### para eventualmente (cuando ya funcione) meterlo todo en una función.
+####----------------------------------------------------------------------
+
+#### Tomamos solamente una ruta para visualizar mejor lo que estamos haciendo
+ruta <- 'RUTA 2'
+ruta1 <- subset(datos.sf, ruta_corre == ruta)
+# ruta1 <- datos.sf
+
+#### El objeto datos.sf es muy complejo, extraemos las coordenadas.
+#### El resultado es una lista con las coordenadas de cada ramal
+coords <-
+  lapply(slot(ruta1, "lines"), function(x)
+    lapply(slot(x, "Lines"),
+           function(y)
+             slot(y, "coords")))
+
+#### Hacemos un dataframe con lo que se aextrajo arriba
+set.seed(1234)
+res2 <- bind_rows(lapply(coords, as.data.frame), .id = 'ramal') %>% 
+  rename(latitud = X1, longitud = X2) %>% 
+  unique() 
+
+res2 %>% head
+
+res3 <- res2 %>%
+  group_by(ramal)
+
+current <- 1
+res3$nodo[1] <- 1
+j <- 1
+
+while(current + j <= nrow(res3)) {
+  
+  distancia <- sqrt((res3$latitud[current + j] - res3$latitud[current])^2 + (res3$longitud[current + j] - res3$longitud[current])^2)
+  
+  print(current)
+  
+  if (distancia < 0.01){
+    res3$nodo[current + j] <- 0
+    j <- j + 1
+  } else  {
+    res3$nodo[current + j] <- 1
+    current <- current + j
+    j <- 1
+  }
+  
+}
+
+res_filt <- res3 %>% 
+  filter(nodo == 1)
+
+res_filt %<>% 
+  mutate(lat_lag = lag(latitud),
+         lon_lag = lag(longitud)) %>% 
+  na.omit()
+
+res_filt %>% head
+
+froms = paste(res_filt$latitud, res_filt$longitud)
+tos = paste(res_filt$lat_lag, res_filt$lon_lag)
+
+graph = graph.edgelist(cbind(froms, tos), directed = FALSE)
+graph_from_edgelist(cbind(froms, tos))
+
+imps <- graph %>% as_tbl_graph() %>% 
+  activate(edges) %>%
+  mutate(weight = 1) %>%
+  activate(nodes) %>% 
+  mutate(importancia = centrality_betweenness(directed = F)) %>% 
+  arrange(-importancia) 
+
+
+importancias <- imps %>% as.data.frame() %>% 
+  separate(name, c('latitud', 'longitud'), sep = ' ') %>% 
+  mutate_at(vars(latitud, longitud), funs(as.numeric))
+
+res3 %>% 
+  ggplot(aes(latitud, longitud)) +
+  geom_path(aes(color = ramal), size = 1.3, alpha = 0.4) + 
+  geom_point(data = importancias,
+             aes(latitud, longitud, size = importancia),
+             alpha = 0.3) +
+  coord_equal() +
+  theme_minimal() +
+  theme(panel.grid = element_blank(), 
+        legend.position = 'none',
+        axis.text = element_blank(),
+        axis.title = element_blank())
+
+importancias$importancia %>% summary
+
+ggraph(graph, layout = 'fr') +
+  geom_edge_link(alpha=0.2) +
+  geom_node_point(colour = 'salmon') +
+  theme_graph(base_family = 'sans')
+
+ggsave(file = 'cerebro.png', width = 12, height = 12, units = 'cm')
--- scripts/01_experimentos.R
+++ scripts/01_experimentos.R
@@ -1,101 +1,129 @@
-# cargamos el paquete RgoogleMaps
+#### ----------------------------------------------------------------------
+####
+#### TODO: 
+####    + Encontrar una forma de obtener subconjuntos de coordenadas
+####      y utilizar la función graph.edgelist para rifarnos.
+####    - Buscar datos de dos ciudades más
+####    - Sumarizar todo en una función o rutina}
+
+####
+#### ----------------------------------------------------------------------
 library('tidyverse')
 library('RgoogleMaps')
 library('readxl')
 library('rgdal')
 library('stplanr')
-library('rjson')
+library('rjson') 
 library('ggmap')
 
-# importamos los datos de las lineas de metro
+# importamos los datos de las rutas
 datos <- read_excel("data/rutas-y-corredores-del-transporte-publico-concesionado.xlsx")
 
+# Lo mismo pero en shapefile, veremos con cuál de los dos conviene trabajar
 datos.sf <- 
   readOGR("data/prueba/rutas-y-corredores-del-transporte-publico-concesionado.shp")
 
 rawplot <- plot(datos.sf)
 
-ruta <- 'RUTA 1'
+
+#### ---------------------------------------------------------------------
+#### La idea aquí es ir haciendo pruebas con subconjuntos de los datos,
+#### para eventualmente (cuando ya funcione) meterlo todo en una función.
+####----------------------------------------------------------------------
+
+#### Tomamos solamente una ruta para visualizar mejor lo que estamos haciendo
+ruta <- 'RUTA 2'
 ruta1 <- subset(datos.sf, ruta_corre == ruta)
-# ruta1 <- subset(datos.sf, ruta_corre == ruta & runif(2311) < 0.5)
 # ruta1 <- datos.sf
 
-res <-
+#### El objeto datos.sf es muy complejo, extraemos las coordenadas.
+#### El resultado es una lista con las coordenadas de cada ramal
+coords <-
   lapply(slot(ruta1, "lines"), function(x)
     lapply(slot(x, "Lines"),
            function(y)
              slot(y, "coords")))
 
+#### Hacemos un dataframe con lo que se aextrajo arriba
 set.seed(1234)
-res2 <- bind_rows(lapply(res, as.data.frame), .id = 'subruta') %>% 
+res2 <- bind_rows(lapply(coords, as.data.frame), .id = 'ramal') %>% 
   rename(latitud = X1, longitud = X2) %>% 
   # filter(runif(nrow(.)) < 0.1) %>% 
-  group_by(subruta) %>%
-  mutate(latitud = round(latitud, 3),
-         longitud = round(longitud, 3)) %>%
-  ungroup() %>%
+  # group_by(ramal) %>%
+  # mutate(latitud = round(latitud, 3),
+  #        longitud = round(longitud, 3)) %>%
+  # ungroup() %>%
   unique() 
 
-res2 %>% select(-subruta) %>% unique() %>% dim
-res2$subruta %>% unique %>% length
+res2 %>% select(-ramal) %>% unique() %>% dim
+res2$ramal %>% unique %>% length
+
+res2
 
 
-
-# Creamos grafo y calculamos importancias
+#### Ahora vamos a probar otra cosa
 
 require('rgeos')
 require('igraph')
+library('ggraph')
+library('tidygraph')
 
-buildTopo <- function(lines){
+#### Esta función la encontré en interné. Recibe un SpatialLineDataFrame,
+#### calcula los nodos a partir de las intersecciones y contruye un igraph.
+buildTopo <- function(lines) {
+  g = gIntersection(lines, lines)
   
-  g = gIntersection(lines,lines)
-  edges = 
-    do.call(rbind,lapply(g@lines[[1]]@Lines,function(ls){as.vector(t(ls@coords))}))
-  lengths = sqrt((edges[,1]-edges[,3])^2+(edges[,2]-edges[,4])^2)
+  edges =
+    do.call(rbind, lapply(g@lines[[1]]@Lines, function(ls) {
+      as.vector(t(ls@coords))
+    }))
   
-  froms = paste(edges[,1],edges[,2])
-  tos = paste(edges[,3],edges[,4])
+  lengths = sqrt((edges[, 1] - edges[, 3]) ^ 2 + (edges[, 2] - edges[, 4]) ^ 2)
   
-  graph = graph.edgelist(cbind(froms,tos),directed=FALSE)
-  E(graph)$weight=lengths
+  froms = paste(edges[, 1], edges[, 2])
+  tos = paste(edges[, 3], edges[, 4])
+  
+  #### Esta es la parte que nos va a ser súper útil
+  graph = graph.edgelist(cbind(froms, tos), directed = FALSE)
+  E(graph)$weight = lengths
+  
   return(graph)
   
 }
 
-dat_g <- buildTopo(ruta1)
+datos.graf <- buildTopo(ruta1)
 
-library('ggraph')
-library('tidygraph')
-
-imps <- dat_g %>% as_tbl_graph() %>% 
+#### Ya que tenemos un grafo podemos hacer lo de siempre con él, como
+#### calcular importancias 
+imps <- datos.graf %>% as_tbl_graph() %>% 
   activate(edges) %>%
   mutate(weight = 1) %>%
-  # filter(runif(imps %>% activate(edges) %>% as.data.frame() %>% nrow) < 0.1) %>% 
   activate(nodes) %>% 
-  # filter(runif(imps %>% activate(nodes) %>% as.data.frame() %>% nrow) < 0.1) %>% 
   mutate(importancia = centrality_betweenness()) %>% 
   arrange(-importancia) 
 
-
+imps %>% filter(importancia == 6423464)
 
 
 importancias <- imps %>% as.data.frame() %>% 
   separate(name, c('latitud', 'longitud'), sep = ' ') %>% 
   mutate_at(vars(latitud, longitud), funs(as.numeric)) #%>% 
-mutate(latitud = round(latitud, 4),
-       longitud = round(longitud, 4)) #%>%
+  mutate(latitud = round(latitud, 4),
+         longitud = round(longitud, 4)) #%>%
 # group_by(latitud, longitud) %>% 
 # summarise(importancia = mean(importancia)) %>% 
 # ungroup()
 
 
 
-# Graficamos
-
+#### Graficamos con fondo blanco
+  
 res2 %>% 
   ggplot(aes(latitud, longitud)) +
-  geom_path(aes(color = subruta), size = 1.3, alpha = 0.4) + 
-  # geom_point(data = importancias, aes(latitud, longitud, size = importancia)) +
+  geom_path(aes(group = ramal), size = 1.3, alpha = 1) + 
+  # geom_point(data = importancias, 
+  #            aes(latitud, longitud, size = importancia),
+  #            alpha = 0.3) +
   coord_equal() +
   theme_minimal() +
   theme(panel.grid = element_blank(), 
@@ -103,17 +131,12 @@ res2 %>%
         axis.text = element_blank(),
         axis.title = element_blank())
 
-# ggraph(imps, layout = 'linear', circular = TRUE) +
-#   geom_edge_link(color = '#24CBE5')  +
-#   geom_node_point(aes(size = importancia), color = 'salmon', fill = 'black') +
-#   # geom_node_text(aes(label = name), nudge_y = 0.2, size = 3, repel = T) +
-#   theme_graph(base_family = 'sans') +
-#   theme(legend.position = 'bottom')
-
-ggsave('raw.png', width = 20, height = 20, units = 'in')
+# ggsave('raw.png', width = 20, height = 20, units = 'in')
 
 
-qmplot(latitud, longitud, data = res2, color = factor(subruta),
+#### COn mapa de fondo
+
+qmplot(latitud, longitud, data = res2, color = factor(ramal),
        maptype = 'watercolor', geom = 'blank') +
   geom_path(size = 1) +
   theme(legend.position = 'none')
